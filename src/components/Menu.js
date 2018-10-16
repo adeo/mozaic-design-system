b1922d67098b693af1871f822643cb37c800edf7
GaelBoyenval <gaelboyenval@macbook-pro-de-gaelboyenval-1.home>
2018-10-16 12:29:10 +0200

    feat: add menu and subfolder

---
+++ src/components/Menu.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Menu extends PureComponent {
+  static propTypes = {}
+
+  render() {
+    return <div>Hello menu</div>
+  }
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import Menu from './Menu'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -29,6 +30,7 @@ const Layout = ({ children }) => (
           <html lang="en" />
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
+        <Menu data={data.allFile} />
         <div
           style={{
             margin: '0 auto',
