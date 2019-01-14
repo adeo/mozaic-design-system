6482105b3736030867e3f9b1874c179bfd7fb56d
gael-boyenval <gaelboyenval@icloud.com>
2019-01-14 12:06:20 +0100

    feat(styleguide:design-tokens): add design tokens from LMFR and export to multiple formats

--- src/tokens/tokens.module.js
+++
@@ -1,4 +0,0 @@
-/* what is the value, expressed in px of 1rem in your stack */
-export const localRemValue = 16;
-/* the most valuable global sizing unit - do not override this value */
-export const magicUnit = 16;
