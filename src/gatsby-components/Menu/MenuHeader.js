d7a90faa0dad15202028b2669e0c6ce55fcec539
gael-boyenval <gaelboyenval@icloud.com>
2019-01-24 16:14:24 +0100

    feat(styleguide/version): create a select with versions and navigation between them (#108)

--- src/gatsby-components/Menu/MenuHeader.js
+++ src/gatsby-components/Menu/MenuHeader.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 
+import MenuVersionSelect from './MenuVersionSelect'
+
 const Header = styled.div`
   background: #f5f5f5;
   padding: 30px 15px;
@@ -12,13 +14,23 @@ const Header = styled.div`
   text-transform: uppercase;
 `
 
-const MenuHeader = ({ siteTitle }) => (
+const MenuHeader = ({ siteTitle, githubReleases }) => (
   <Header>
     <Link to="/">{siteTitle}</Link>
+    <MenuVersionSelect githubReleases={githubReleases} />
   </Header>
 )
+
 MenuHeader.propTypes = {
   siteTitle: PropTypes.string.isRequired,
+  githubReleases: PropTypes.arrayOf(
+    PropTypes.shape({
+      node: PropTypes.shape({
+        url: PropTypes.string.isRequired,
+        tagName: PropTypes.string.isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
 }
 
 export default MenuHeader
