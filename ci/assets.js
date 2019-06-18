23160ad0704022d2e67908f3416c1c3d32e89c1c
Jonathan Jalouzot <jonathan.jalouzot@gmail.com>
2019-06-18 16:33:34 +0200

    feat(assets): add command to upload assets

---
+++ ci/assets.js
@@ -0,0 +1,75 @@
+const fs = require('fs')
+const AdmZip = require('adm-zip')
+const request = require('request-promise')
+
+const stat = fs.statSync
+
+const lernaData = fs.readFileSync('./lerna.json')
+const lerna = JSON.parse(lernaData)
+const rawdata = fs.readFileSync('./assets.config.json')
+const assets = JSON.parse(rawdata)
+
+function newArchive(zipFileName, pathNames) {
+  const zip = new AdmZip()
+
+  pathNames.forEach(path => {
+    const p = stat(path)
+    if (p.isFile()) {
+      zip.addLocalFile(path)
+    } else if (p.isDirectory()) {
+      const split = path.split('/')
+      zip.addLocalFolder(path, split[split.length - 1])
+    }
+  })
+
+  zip.writeZip(zipFileName)
+}
+
+async function getReleaseByName() {
+  var options = {
+    method: 'GET',
+    url: `https://api.github.com/repos/adeo/design-system--styleguide/releases/tags/${
+      lerna.version
+    }`,
+    headers: {
+      'User-Agent': 'Docker',
+      authorization: 'bearer debe49d99c31a1b1079160215b4568cef48c0298',
+      'content-type': 'application/json',
+    },
+  }
+
+  const release = await request(options)
+  return release
+}
+
+async function sendAssetForRelease(idRelease, name) {
+  var stats = fs.statSync(name)
+
+  var options = {
+    method: 'POST',
+    url: `https://uploads.github.com/repos/adeo/design-system--styleguide/releases/${idRelease}/assets`,
+    qs: { name: name },
+    headers: {
+      'User-Agent': 'Docker',
+      authorization: 'bearer debe49d99c31a1b1079160215b4568cef48c0298',
+      'content-type': 'application/zip',
+      'Content-Length': stats.size,
+    },
+
+    body: fs.createReadStream(name),
+  }
+
+  const req = await request(options)
+}
+
+async function createAssets() {
+  const release = await getReleaseByName()
+  const idRelease = JSON.parse(release).id
+
+  assets.assets.forEach(function(asset) {
+    newArchive(asset.name, asset.folders)
+    sendAssetForRelease(idRelease, asset.name)
+  })
+}
+
+createAssets()
