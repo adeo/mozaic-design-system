ab461bdfe2b0b8b5916b3472ffb17451f2707ec7
gael-boyenval <gaelboyenval@icloud.com>
2019-06-27 17:54:05 +0200

    feat(styleguide): add mix panel to the design system (#276)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,4 +5,12 @@ module.exports = {
   __experimentalThemes: [
     { resolve: '@mozaic-ds/gatsby-theme-styleguide', options: {} },
   ],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-mixpanel',
+      options: {
+        apiToken: '5c0a2fe71e9fd4380c29b5ea0aee2382', // required
+      },
+    },
+  ],
 }
