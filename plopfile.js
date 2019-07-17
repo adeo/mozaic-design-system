39004a499776c25279ca37638ccbb682313737ee
gael-boyenval <gaelboyenval@icloud.com>
2019-07-17 08:54:38 +0200

    feat(styleguide/generator): install plop to generate new patterns (#287)

---
+++ plopfile.js
@@ -0,0 +1,32 @@
+module.exports = plop => {
+  // controller generator
+  plop.setGenerator('pattern', {
+    description: 'create a new pattern',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'what is the pattern name please',
+      },
+      {
+        type: 'input',
+        name: 'path',
+        message:
+          'where do you want to add the pattern (from the pages directory)',
+      },
+      {
+        type: 'input',
+        name: 'order',
+        message: "witch item's order (integer)",
+      },
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: 'src/pages/{{path}}/{{name}}',
+        templateFiles: 'plop-templates/pattern/**/*',
+        base: 'plop-templates/pattern/',
+      },
+    ],
+  })
+}
