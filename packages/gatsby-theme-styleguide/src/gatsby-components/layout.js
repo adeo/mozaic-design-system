be75b8cd3d0787a885d525e93a8dae64a8c63499
Remy Ponso <remy.ponso@gmail.com>
2019-10-15 13:00:27 -0300

    feat(website/feedback-module): add feedback module

--- packages/gatsby-theme-styleguide/src/gatsby-components/layout.js
+++ packages/gatsby-theme-styleguide/src/gatsby-components/layout.js
@@ -55,6 +55,20 @@ const Layout = ({ children, location, tableOfContents }) => {
             ]}
           >
             <html lang="en" />
+            <script>
+              {`
+              <!-- Hotjar Tracking Code for http://mozaic.adeo.cloud -->
+                (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:1528294,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+              `}
+            </script>
           </Helmet>
           <MenuContainer>
             <Menu
