73bcf6d4ff1dea504e0f1355d9baff3bbafded3c
mohamedMok <sahilgupta@qantas.com.au>
2019-12-04 11:19:35 +0100

    fix(chore/test): add coverage for repository outside packages/

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,12 @@ const base = require('./jest.config.base.js')
 
 module.exports = {
   ...base,
-  projects: ['<rootDir>', '<rootDir>/packages/*/jest.config.js'],
+  projects: [
+    '<rootDir>/',
+    '<rootDir>/iconCompiler/jest.config.js',
+    '<rootDir>/ci/jest.config.js',
+    '<rootDir>/packages/*/jest.config.js',
+  ],
   cacheDirectory: '.jest-cache',
   coverageDirectory: '.jest-coverage',
   coverageReporters: ['html', 'text'],
