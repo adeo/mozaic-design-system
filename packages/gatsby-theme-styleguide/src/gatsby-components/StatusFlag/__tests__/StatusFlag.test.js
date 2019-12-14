d0827c52c5f3031c57259e3c2bdb54792c214645
mohamedMok <sahilgupta@qantas.com.au>
2019-12-14 10:26:21 +0100

    feat(website/header): correct typo add test on status flag

--- packages/gatsby-theme-styleguide/src/gatsby-components/StatusFlag/__tests__/StatusFlag.test.js
+++ packages/gatsby-theme-styleguide/src/gatsby-components/StatusFlag/__tests__/StatusFlag.test.js
@@ -2,24 +2,26 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import StatusFlag from '../StatusFlag'
 
-// TODO: test status color
 describe(`<StatusFlag>`, () => {
   it(`should render correctly with a wip status`, () => {
     const tree = renderer.create(<StatusFlag status="wip" />).toJSON()
     expect(tree).toMatchSnapshot()
     expect(tree).toHaveStyleRule('border', 'solid 1px transparent')
+    expect(tree).toHaveStyleRule('border-color', '#e3cb58')
   })
 
   it(`should render correctly with a bêta status`, () => {
     const tree = renderer.create(<StatusFlag status="bêta" />).toJSON()
     expect(tree).toMatchSnapshot()
     expect(tree).toHaveStyleRule('border', 'solid 1px transparent')
+    expect(tree).toHaveStyleRule('border-color', '#e35876')
   })
 
   it(`should render correctly with a stable status`, () => {
     const tree = renderer.create(<StatusFlag status="stable" />).toJSON()
     expect(tree).toMatchSnapshot()
     expect(tree).toHaveStyleRule('border', 'solid 1px transparent')
+    expect(tree).toHaveStyleRule('border-color', '#58e3b7')
   })
 
   it(`should should not render anything using an unsuported status prop`, () => {
