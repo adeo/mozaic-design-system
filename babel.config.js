e3817f402a78ca6051af1b90bd9404522e1a1aaa
Trésor ILOYI <tresor.iloyi@gmail.com>
2021-03-02 16:19:10 +0100

    fix(gatsby): add babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react', 'babel-preset-gatsby'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
+}
