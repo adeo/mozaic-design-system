39aaa1206116a1abfbf328c89141f09192f3190c
Mohamed Mokhtari <mohamed.mokhtari.pro@gmail.com>
2021-05-12 10:09:17 +0200

    feat(foundations/icons): add mozaic svelte icon components (#824)

---
+++ packages/icons/svelte/ArrowArrowBottom24.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let id = undefined;
+  export let style = undefined;
+  export let className = undefined;
+  export let fill = undefined;
+  export let size = "1.5rem";
+</script>
+
+<svg width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12.71 17.21a1 1 0 01-1.42 0l-9-9a1 1 0 111.42-1.42l8.29 8.3 8.29-8.3a1 1 0 111.42 1.42z"/></svg>
---
+++ packages/icons/svelte/ViewStarHalf24.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let id = undefined;
+  export let style = undefined;
+  export let className = undefined;
+  export let fill = undefined;
+  export let size = "1.5rem";
+</script>
+
+<svg width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 18.58V2L8.91 8.58 2 9.64l5 5.12L5.82 22 12 18.58z"/></svg>
