17b72f4cac70261c182cdd2aa5336d47800a313f
Mohamed Mokhtari <mohamed.mokhtari.pro@gmail.com>
2021-11-10 14:58:01 +0100

    feat(foundations/icons): run svgoplugin (#956)

--- packages/icons/svelte/DisplayDisplayProductX4Filled64.svelte
+++ packages/icons/svelte/DisplayDisplayProductX4Filled64.svelte
@@ -7,4 +7,4 @@
   export let size = "4rem";
 </script>
 
-<svg width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><rect x="5" y="5" width="25" height="25" rx="2"/><rect x="34" y="5" width="25" height="25" rx="2"/><rect x="5" y="34" width="25" height="25" rx="2"/><rect x="34" y="34" width="25" height="25" rx="2"/></svg>
+<svg width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><rect width="25" height="25" x="5" y="5" rx="2"/><rect width="25" height="25" x="34" y="5" rx="2"/><rect width="25" height="25" x="5" y="34" rx="2"/><rect width="25" height="25" x="34" y="34" rx="2"/></svg>
--- packages/icons/svelte/ViewStarFull32.svelte
+++ packages/icons/svelte/ViewStarFull32.svelte
@@ -7,4 +7,4 @@
   export let size = "2rem";
 </script>
 
-<svg width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M16 24.56L7.97 29l1.53-9.41L3 12.93l8.98-1.37L16 3l4.02 8.56L29 12.93l-6.5 6.66L24.03 29 16 24.56z"/></svg>
+<svg width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><polygon points="16 24.56 7.97 29 9.5 19.59 3 12.93 11.98 11.56 16 3 20.02 11.56 29 12.93 22.5 19.59 24.03 29 16 24.56"/></svg>
