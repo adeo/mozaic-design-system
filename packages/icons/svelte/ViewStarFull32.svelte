dd70e38e271126607752b39bb1f4be8bc4dcfc66
Charles-Antoine Queste <43212270+caqueste@users.noreply.github.com>
2022-04-12 17:29:19 +0200

    refactor(foundations/icons): rework star icons (#1090)

--- packages/icons/svelte/ViewStarFull32.svelte
+++ packages/icons/svelte/ViewStarFull32.svelte
@@ -7,4 +7,4 @@
   export let size = "2rem";
 </script>
 
-<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M16 24.56L7.97 29l1.53-9.41L3 12.93l8.98-1.37L16 3l4.02 8.56L29 12.93l-6.5 6.66L24.03 29 16 24.56z"/></svg>
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill-rule="evenodd" d="M16.908 2.914a1 1 0 00-1.816 0l-3.772 8.173-8.794 1.256a1 1 0 00-.492 1.764l6.824 5.584-2.486 8.7a1 1 0 001.486 1.127L16 24.508l8.143 5.01a1 1 0 001.486-1.126l-2.487-8.701 6.825-5.584a1 1 0 00-.492-1.764l-8.795-1.256-3.772-8.173z"/></svg>
