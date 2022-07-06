11d2c146c2e49f23c2e2c6a4901c7d7d55395f65
Trésor ILOYI <tiloyi@users.noreply.github.com>
2022-07-06 09:45:35 +0200

    feat(tools): update the dependencies and the code of the `@mozaic-ds/stylelint-plugin-mozaic` package (#1157)

--- packages/stylelint-plugin-mozaic/index.js
+++ packages/stylelint-plugin-mozaic/index.js
@@ -1,37 +1,41 @@
 const _ = require('lodash')
-const stylelint = require('stylelint')
 
-const checkCase = require('./lib/checkCase')
+const stylelint = require('stylelint')
+const ruleName = 'plugin/mozaic-bem-pattern'
+
 const getSelector = require('./lib/getSelectors')
 const splitSelector = require('./lib/splitSelector')
-
-const ruleName = 'plugin/mozaic-bem-pattern'
+const checkCase = require('./lib/checkCase')
 
 const isStringOrRegExp = (x) => {
   return _.isString(x) || _.isRegExp(x)
 }
 
-module.exports = stylelint.createPlugin(
-  ruleName,
-  (options, secondaryOptionObject) => {
-    return (root, result) => {
-      const validOptions = stylelint.utils.validateOptions(result, ruleName, {
+module.exports = stylelint.createPlugin(ruleName, (options) => {
+  return (postcssRoot, postcssResult) => {
+    const validOptions = stylelint.utils.validateOptions(
+      postcssResult,
+      ruleName,
+      {
         actual: options,
         possible: {
           caseStyle: [isStringOrRegExp],
           bemEntitiesDelimiters: [_.isObject],
           prefixes: [_.isString],
         },
-      })
-      if (!validOptions) return
+      }
+    )
 
-      root.walkRules((rule) => {
-        const selector = getSelector(rule, result)
-        const splitedSelector = splitSelector(selector, options)
-        checkCase(splitedSelector, rule, result, ruleName, options)
-      })
-    }
+    if (!validOptions) return
+
+    const excludedNestedPropertyDeclarations = /[^(margin|padding):$]/i
+
+    postcssRoot.walkRules(excludedNestedPropertyDeclarations, (rule) => {
+      const selector = getSelector(rule, postcssResult)
+      const splitedSelector = splitSelector(selector, options)
+      checkCase(splitedSelector, rule, postcssResult, ruleName, options)
+    })
   }
-)
+})
 
 module.exports.ruleName = ruleName
