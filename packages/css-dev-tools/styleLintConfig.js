b64d12c6d1da67b8a7666ebfbb0a1f6cb05e930e
Trésor ILOYI <tiloyi@users.noreply.github.com>
2022-09-27 11:33:50 +0200

    feat(styles): improve styles to conform to stylelint rules (#1187)
    Resolves #1062

--- packages/css-dev-tools/styleLintConfig.js
+++ packages/css-dev-tools/styleLintConfig.js
@@ -35,28 +35,14 @@ module.exports = {
   extends: ['stylelint-config-standard-scss'],
   plugins: ['@mozaic-ds/stylelint-plugin-mozaic'],
   rules: {
-    'at-rule-no-unknown': null,
-    'max-empty-lines': null,
     'plugin/mozaic-bem-pattern': [pickConfig()],
-    'no-invalid-position-at-import-rule': null,
-    'max-line-length': null,
+    // rules from Stylelint
     'alpha-value-notation': null,
     'selector-class-pattern': null,
+    'selector-id-pattern': null,
+    'selector-not-notation': 'simple',
+    // rules from stylelint-scss
     'scss/no-global-function-names': null,
-    // TODO: the following rules should be removed from the file when switching to @next
-    'scss/at-mixin-argumentless-call-parentheses': null,
-    'scss/at-import-partial-extension': null,
-    'scss/at-import-no-partial-leading-underscore': null,
-    'scss/double-slash-comment-empty-line-before': null,
-    'scss/at-rule-conditional-no-parentheses': null,
-    'custom-property-no-missing-var-function': null,
-    'string-quotes': null,
-    'at-rule-empty-line-before': null,
-    'color-function-notation': null,
-    'function-url-quotes': null,
-    'value-keyword-case': null,
-    'declaration-colon-newline-after': null,
-    'rule-empty-line-before': null,
-    'no-descending-specificity': null,
+    'scss/at-mixin-argumentless-call-parentheses': 'always',
   },
 }
