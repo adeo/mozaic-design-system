17b8b0e0bd1463ddc73c548cce2d2d50b2c9e16c
Trésor ILOYI <tiloyi@users.noreply.github.com>
2022-12-29 19:31:53 +0100

    feat(tools/tokens): build all preset within the tokens package (#1299)
    Resolves #1232
    
    Co-authored-by: Mohamed Mokhtari <mohamed.mokhtari.pro@gmail.com>

--- packages/tokens/tokensBuild.js
+++ packages/tokens/tokensBuild.js
@@ -1,4 +1,24 @@
-const config = require('./config.js')
-const StyleDictionary = require('style-dictionary').extend(config)
+const StyleDictionary = require('style-dictionary')
+const getConfig = require('./config.js')
+const brands = [
+  {
+    brand: 'lm',
+    tokens: 'src/tokens/leroymerlin/**/*.json',
+  },
+  {
+    brand: 'adeo',
+    tokens: 'src/tokens/adeo/**/*.json',
+  },
+  {
+    brand: 'bricoman',
+    tokens: 'src/tokens/bricoman/**/*.json',
+  },
+]
 
-module.exports = () => StyleDictionary.buildAllPlatforms()
+module.exports = () => {
+  brands.forEach((brand) => {
+    const brandConfig = getConfig(brand)
+
+    StyleDictionary.extend(brandConfig).buildAllPlatforms()
+  })
+}
