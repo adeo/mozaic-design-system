caf45b863deb37578190e05d8567a8c1ed499c66
Mohamed Mokhtari <mohamed.mokhtari.pro@gmail.com>
2023-02-13 16:59:14 +0100

    docs(tabs): use Mozaic tabs in docs (#1340)

--- packages/gatsby-theme-styleguide/src/gatsby-components/PageTabs/PageTabs.js
+++ packages/gatsby-theme-styleguide/src/gatsby-components/PageTabs/PageTabs.js
@@ -2,61 +2,18 @@ import React, { PureComponent } from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
 import { MagicUnit } from '@mozaic-ds/tokens/build/lm/js/tokens.js'
+import './styles.scss'
 
 const Tabs = styled.div`
-  position: sticky;
-  width: 100%;
-  z-index: 1;
-  top: 0;
-  border-bottom: solid 1px #000;
-  background: white;
-
-  @media screen and (max-width: 767px) {
-    overflow-x: auto;
-  }
+padding-left: 2rem;
 `
 
-const TabsWrapper = styled.div`
-  display: inline-flex;
-  padding-left: ${MagicUnit * 1.5}rem;
-  max-width: ${MagicUnit * 52}rem;
-
-  @media screen and (min-width: 768px) {
-    padding-left: ${MagicUnit * 3}rem;
-  }
+const TabsWrapper = styled.ul`
 `
 
-const TabItem = styled.div``
+const TabItem = styled.li``
 
-const TabLink = styled(Link)`
-  color: #000;
-  display: block;
-  position: relative;
-  padding: ${MagicUnit}rem ${MagicUnit * 1.5}rem;
-
-  &::after {
-    content: '';
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 0.25rem;
-  }
-
-  &:hover:not(.is-active) {
-    &::after {
-      background: rgba(0, 0, 0, 0.5);
-    }
-  }
-
-  &.is-active {
-    font-weight: bold;
-
-    &::after {
-      background: rgba(0, 0, 0, 1);
-    }
-  }
-`
+const TabLink = styled(Link)``
 
 class PageTabs extends PureComponent {
   orderPageTab = (pageTabs) => {
@@ -89,18 +46,19 @@ class PageTabs extends PureComponent {
     const { samePageTabs } = this.props
     const cleanTabs = this.orderPageTab(samePageTabs)
     return (
-      <Tabs id="page_tabs_menu">
-        <TabsWrapper>
+      <Tabs className="mc-tabs  mc-tabs--full" id="page_tabs_menu">
+        <TabsWrapper className="mc-tabs__nav">
           {cleanTabs.map((node) => (
-            <TabItem key={node.slug}>
+            <TabItem key={node.slug} className="mc-tabs__item">
               <TabLink
                 to={node.slug}
-                activeClassName="is-active"
+                activeClassName="mc-tabs__element--selected"
+                className="mc-tabs__element"
                 state={{
                   isCode: node.title === 'Code',
                 }}
               >
-                {node.title}
+                <span className="mc-tabs__text">{node.title}</span>
               </TabLink>
             </TabItem>
           ))}
