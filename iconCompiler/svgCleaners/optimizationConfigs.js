17ee591bb696aeed0824ca683f608285724d7d25
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-02-22 14:32:39 +0100

    feat(icons): add v2 icons

--- iconCompiler/svgCleaners/optimizationConfigs.js
+++ iconCompiler/svgCleaners/optimizationConfigs.js
@@ -1,93 +1,36 @@
 module.exports['monochromOptim'] = {
-  custom: (data) =>
-    data
-      .replace(/<style>(.*)<\/style>/g, '')
-      .replace(/<defs>(.*)<\/defs>/g, '')
-      .replace(/class="[a-zA-Z0-9:;\.\s\(\)\-\,]*"/gi, '')
-      .replace(/data-name="[a-zA-Z0-9:;\.\s\(\)\-\,\/]*"/gi, '')
-      .replace(/fill="[a-zA-Z0 -9:;\.\s\(\)\-\,]*"/gi, '')
-      .replace(/<g id="Square">.*?<\/g>/gi, ''),
-
-  svgoPlugins: [
-    { cleanupAttrs: true },
-    { removeDoctype: true },
-    { removeXMLProcInst: true },
-    { removeComments: true },
-    { removeMetadata: true },
-    { removeTitle: true },
-    { removeDesc: true },
-    { removeUselessDefs: true },
-    { removeEditorsNSData: true },
-    { removeEmptyAttrs: true },
-    { removeHiddenElems: true },
-    { removeEmptyText: true },
-    { removeEmptyContainers: true },
-    { removeViewBox: false },
-    { minifyStyles: true },
-    { cleanupEnableBackground: true },
-    { convertStyleToAttrs: true },
-    { convertColors: true },
-    { convertPathData: true },
-    { convertTransform: true },
-    { removeUnknownsAndDefaults: true },
-    { removeNonInheritableGroupAttrs: true },
-    { removeUselessStrokeAndFill: true },
-    { removeUnusedNS: true },
-    { cleanupIDs: true },
-    { cleanupNumericValues: false },
-    { moveElemsAttrsToGroup: true },
-    { moveGroupAttrsToElems: true },
-    { collapseGroups: true },
-    { removeRasterImages: false },
-    { mergePaths: true },
-    { convertShapeToPath: false },
-    { sortAttrs: true },
-    { removeDimensions: true },
+  plugins: [
+    {
+      name: 'preset-default',
+      custom: {
+        type: 'full',
+        descritpion: 'Clean svg',
+        fn: (data) =>
+          data
+            .replace(/<style>(.*)<\/style>/g, '')
+            .replace(/<defs>(.*)<\/defs>/g, '')
+            .replace(/<title>(.*)<\/title>/g, '')
+            .replace(/class="[a-zA-Z0-9:;\.\s\(\)\-\,]*"/gi, '')
+            .replace(/data-name="[a-zA-Z0-9:;\.\s\(\)\-\,\/]*"/gi, '')
+            .replace(/fill="[a-zA-Z0 -9:;\.\s\(\)\-\,]*"/gi, '')
+      },
+    },
   ],
 }
 
 module.exports['colorOptim'] = {
-  custom: (data) =>
-    data
-      .replace(/<defs><\/defs>/g, '')
-      .replace(/data-name="[a-zA-Z0-9:;\.\s\(\)\-\,\/]*"/gi, '')
-      .replace(/<g id="Square">.*?<\/g>/gi, ''),
-
-  svgoPlugins: [
-    { cleanupAttrs: true },
-    { removeDoctype: true },
-    { removeXMLProcInst: true },
-    { removeComments: true },
-    { removeMetadata: true },
-    { removeTitle: true },
-    { removeDesc: true },
-    { removeUselessDefs: true },
-    { removeEditorsNSData: true },
-    { removeEmptyAttrs: true },
-    { removeHiddenElems: true },
-    { removeEmptyText: true },
-    { removeEmptyContainers: true },
-    { removeViewBox: false },
-    { cleanupEnableBackground: true },
-    { convertStyleToAttrs: true },
-    { convertColors: true },
-    { minifyStyles: true },
-    { convertPathData: true },
-    { convertTransform: true },
-    { removeUnknownsAndDefaults: true },
-    { removeNonInheritableGroupAttrs: true },
-    { removeUselessStrokeAndFill: true },
-    { removeUnusedNS: true },
-    { cleanupIDs: true },
-    { cleanupNumericValues: false },
-    { moveElemsAttrsToGroup: true },
-    { moveGroupAttrsToElems: true },
-    { collapseGroups: true },
-    { removeRasterImages: false },
-    { mergePaths: true },
-    { convertShapeToPath: false },
-    { sortAttrs: true },
-    { removeDimensions: true },
-    { inlineStyles: true },
+  plugins: [
+    {
+      custom: {
+        type: 'full',
+        descritpion: 'Clean svg',
+        // params: data,
+        fn: (data) =>
+          data
+            .replace(/<defs><\/defs>/g, '')
+            .replace(/data-name="[a-zA-Z0-9:;\.\s\(\)\-\,\/]*"/gi, '')
+            .replace(/<g id="Square">.*?<\/g>/gi, ''),
+      },
+    },
   ],
 }
--- packages/icons/svelte/CardsColorsCheque48.svelte
+++ packages/icons/svelte/CardsColorsCheque48.svelte
@@ -7,4 +7,18 @@
   export let size = "3rem";
 </script>
 
-<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill="url(#a)" d="M4 12a2 2 0 012-2h36a2 2 0 012 2v24a2 2 0 01-2 2H6a2 2 0 01-2-2V12z"/><path fill="#B7EEF5" fill-rule="evenodd" d="M6 10h36a2 2 0 012 2v24a2 2 0 01-2 2H6a2 2 0 01-2-2V12a2 2 0 012-2zm37 2a1 1 0 00-1-1H6a1 1 0 00-1 1v24a1 1 0 001 1h36a1 1 0 001-1V12z"/><path fill="#84DBEB" d="M37 20H24.41l2-2H37a1 1 0 100-2h-8.59l.8-.79a1.004 1.004 0 00-1.42-1.42L25.59 16H11a1 1 0 100 2h12.59l-2 2H11a1 1 0 100 2h8.59l-.8.79a1 1 0 000 1.42 1 1 0 001.42 0l2.2-2.21H37a1 1 0 100-2z"/><path fill="#B7EEF5" d="M10 28a1 1 0 011-1h10a1 1 0 011 1v5a1 1 0 01-1 1H11a1 1 0 01-1-1v-5z"/><path fill="#25A8D0" d="M25 34a1 1 0 01-.93-1.37C26.32 27 27.5 27 28 27s1.38 0 3.09 3.67C32.57 28 33 28 33.5 28c.5 0 1.07 0 3.12 4H38a1 1 0 110 2h-2a1 1 0 01-.89-.55c-.51-1-1.15-2.19-1.61-2.92-.46.73-1.1 1.9-1.61 2.92A1 1 0 0131 34a1 1 0 01-.89-.63A23 23 0 0028 29.25a23 23 0 00-2.07 4.12A1 1 0 0125 34z"/><defs><linearGradient id="a" x1="21.32" x2="46.808" y1="59.637" y2="23.453" gradientUnits="userSpaceOnUse"><stop stop-color="#fff"/><stop offset=".53" stop-color="#F1FDFE"/><stop offset="1" stop-color="#ECFCFD"/></linearGradient></defs></svg>
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+    
+  <path fill="url(#a)" d="M4 12a2 2 0 0 1 2-2h36a2 2 0 0 1 2 2v24a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V12Z"/>
+  <path fill="#B7EEF5" fill-rule="evenodd" d="M6 10h36a2 2 0 0 1 2 2v24a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V12a2 2 0 0 1 2-2Zm37 2a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1v24a1 1 0 0 0 1 1h36a1 1 0 0 0 1-1V12Z"/>
+  <path fill="#84DBEB" d="M37 20H24.41l2-2H37a1 1 0 1 0 0-2h-8.59l.8-.79a1.004 1.004 0 0 0-1.42-1.42L25.59 16H11a1 1 0 1 0 0 2h12.59l-2 2H11a1 1 0 1 0 0 2h8.59l-.8.79a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l2.2-2.21H37a1 1 0 1 0 0-2Z"/>
+  <path fill="#B7EEF5" d="M10 28a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1H11a1 1 0 0 1-1-1v-5Z"/>
+  <path fill="#25A8D0" d="M25 34a1 1 0 0 1-.93-1.37C26.32 27 27.5 27 28 27s1.38 0 3.09 3.67C32.57 28 33 28 33.5 28c.5 0 1.07 0 3.12 4H38a1 1 0 1 1 0 2h-2a1 1 0 0 1-.89-.55c-.51-1-1.15-2.19-1.61-2.92-.46.73-1.1 1.9-1.61 2.92A1 1 0 0 1 31 34a1 1 0 0 1-.89-.63A23 23 0 0 0 28 29.25a23 23 0 0 0-2.07 4.12A1 1 0 0 1 25 34Z"/>
+  <defs>
+    <linearGradient id="a" x1="21.32" x2="46.808" y1="59.637" y2="23.453" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#fff"/>
+      <stop offset=".53" stop-color="#F1FDFE"/>
+      <stop offset="1" stop-color="#ECFCFD"/>
+    </linearGradient>
+  </defs>
+</svg>
