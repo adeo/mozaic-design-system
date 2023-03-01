c0d553bbcb9a82250e033509b34c7dc7538c78c1
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-03-01 12:54:08 +0100

    feat(preset/zodio): load Lato font from Google Fonts

--- packages/gatsby-theme-styleguide/gatsby-node.js
+++ packages/gatsby-theme-styleguide/gatsby-node.js
@@ -116,6 +116,9 @@ const buildHtml = (data) => {
         <head>
           <meta charset="utf-8"><meta http-equiv="x-ua-compatible" content="ie=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+          <link rel="preconnect" href="https://fonts.googleapis.com">
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+          <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
           <style type="text/css">
             * {
               box-sizing: padding-box;
