5d843124bbeffb43de831202f943db755a4c3f20
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-05-29 11:42:21 +0200

    chore(gatsby/babel): remove custom babel config

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', 'babel-preset-gatsby'],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: false }],
-    ['@babel/plugin-proposal-private-methods', { loose: false }],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
-    '@babel/plugin-syntax-dynamic-import',
-  ],
-}
