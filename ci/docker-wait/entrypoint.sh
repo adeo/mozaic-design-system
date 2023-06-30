b720a3571fbd7978787a7d7336c8cb112dbc932b
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-06-30 17:00:11 +0200

    feat(docker): update dockerfile

--- ci/docker-wait/entrypoint.sh
+++ ci/docker-wait/entrypoint.sh
@@ -5,4 +5,8 @@ set -e
 echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 gcloud auth activate-service-account --key-file=$HOME/gcloud.json
 
+apt install nodejs npm 
+
+npm install -g yarn 
+
 sh -c "yarn $*"
