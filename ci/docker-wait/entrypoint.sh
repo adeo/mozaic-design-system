4d842b86d31adbe2ae25d3132424335ce4e86ea2
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-07-03 12:50:08 +0200

    chore(entrypoint): remove apt node install

--- ci/docker-wait/entrypoint.sh
+++ ci/docker-wait/entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 gcloud auth activate-service-account --key-file=$HOME/gcloud.json
 
-apt install nodejs npm 
+# apt install nodejs npm 
 
-npm install -g yarn 
+# npm install -g yarn 
 
 sh -c "yarn $*"
