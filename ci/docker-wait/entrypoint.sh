ce66c7144456de376f0e6b830718c42e72cbb9d8
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-07-03 13:02:22 +0200

    chore(entrypoint): set non-interactive

--- ci/docker-wait/entrypoint.sh
+++ ci/docker-wait/entrypoint.sh
@@ -5,8 +5,9 @@ set -e
 echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 gcloud auth activate-service-account --key-file=$HOME/gcloud.json
 
-# apt install nodejs npm 
+export DEBIAN_FRONTEND=noninteractive
+apt-get -yq install nodejs npm 
 
-# npm install -g yarn 
+npm install -g yarn 
 
 sh -c "yarn $*"
