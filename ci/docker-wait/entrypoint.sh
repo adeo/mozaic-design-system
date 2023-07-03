e3a2020aaf34e690bd3885fea0347de2fa54f2bb
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-07-03 14:06:33 +0200

    chore(entypoint): use latest node

--- ci/docker-wait/entrypoint.sh
+++ ci/docker-wait/entrypoint.sh
@@ -6,6 +6,8 @@ echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 gcloud auth activate-service-account --key-file=$HOME/gcloud.json
 
 export DEBIAN_FRONTEND=noninteractive
+curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
+
 apt-get -yq install nodejs npm 
 
 npm install -g yarn 
