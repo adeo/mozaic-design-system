8a5a1b0b2a3f9c2abd9ab605a53d11912c347f0b
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-07-03 14:41:59 +0200

    chore(entrypoint): specify node 18

--- ci/docker-wait/entrypoint.sh
+++ ci/docker-wait/entrypoint.sh
@@ -6,7 +6,7 @@ echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 gcloud auth activate-service-account --key-file=$HOME/gcloud.json
 
 export DEBIAN_FRONTEND=noninteractive
-curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x  | bash -
 
 apt-get -yq install nodejs npm 
 
