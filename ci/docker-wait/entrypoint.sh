df10c648f5f932f003482d0c360aceb37aecad06
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-07-03 17:45:49 +0200

    chore(dockerfile): install yarn

--- ci/docker-wait/entrypoint.sh
+++ ci/docker-wait/entrypoint.sh
@@ -5,8 +5,4 @@ set -e
 echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 gcloud auth activate-service-account --key-file=$HOME/gcloud.json
 
-export DEBIAN_FRONTEND=noninteractive
-
-npm install -g yarn 
-
 sh -c "yarn $*"
