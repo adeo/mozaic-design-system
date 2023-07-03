a1d510bb7a61c10b6787d5adaaf03bdc525d1915
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-07-03 17:20:39 +0200

    chore(docker): add nvm

--- ci/docker-wait/entrypoint.sh
+++ ci/docker-wait/entrypoint.sh
@@ -6,9 +6,6 @@ echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 gcloud auth activate-service-account --key-file=$HOME/gcloud.json
 
 export DEBIAN_FRONTEND=noninteractive
-curl -fsSL https://deb.nodesource.com/setup_18.x  | bash -
-
-apt-get -yq install nodejs npm 
 
 npm install -g yarn 
 
