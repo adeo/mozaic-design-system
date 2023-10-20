7b76ca8c2af415e4cab096ffb8e0211b7b3ab052
Trésor ILOYI <tiloyi@users.noreply.github.com>
2023-10-20 12:45:13 +0200

    docs(gatsby): using stories to replace custom previews (#1498)

---
+++ packages/gatsby-theme-styleguide/src/gatsby-components/Story/Story.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './story.css'
+
+function Story({ id, width = 800, height = 260, canvas = true }) {
+  const embedRatio = (height / width) * 100
+  const chromaticUrl = 'https://main--651ae46c2a6f4410809759ce.chromatic.com/'
+  const argsStory = `?path=/story/${id}&full=1&shortcuts=false&singleStory=true`
+  const argsCanvas = `iframe.html?id=${id}&viewMode=story`
+  const argsUrl = !canvas ? argsStory : argsCanvas
+  const iframeUrl = chromaticUrl + argsUrl
+
+  return (
+    <>
+      <div
+        className="embed-responsive"
+        style={{
+          '--container-ratio': `${embedRatio}%`,
+        }}
+      >
+        <iframe
+          src={iframeUrl}
+          width={width}
+          height={height}
+          className="embed-responsive-item"
+        ></iframe>
+      </div>
+    </>
+  )
+}
+
+export default Story
