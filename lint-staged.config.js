2b5a6e53f7935317128021a20c540dddd0ce70a6
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-11-06 10:53:01 +0100

    chore: temporarily disable lint-staged for js files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   '*.scss': ['stylelint --fix', 'stylelint', 'git add'],
-  '*.js': [
-    'yarn format',
-    'git add',
-    (filenames) => `jest --coverage --findRelatedTests ${filenames.join(' ')}`,
-  ],
+  // '*.js': [
+  //   'yarn format',
+  //   'git add',
+  //   (filenames) => `jest --coverage --findRelatedTests ${filenames.join(' ')}`,
+  // ],
 }
