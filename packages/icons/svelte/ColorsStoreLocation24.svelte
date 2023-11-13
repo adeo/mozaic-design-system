33e0da603787d854350d06a302ebd927588f926a
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2023-11-13 15:08:30 +0100

    feat(icons): add v2 icons

--- packages/icons/svelte/ColorsStoreLocation24.svelte
+++ packages/icons/svelte/ColorsStoreLocation24.svelte
@@ -7,4 +7,10 @@
   export let size = "1.5rem";
 </script>
 
-<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill-rule="evenodd"><path fill="#006902" d="M12 2a8 8 0 00-8 8c0 4.16 6.54 10.94 7.29 11.7a1 1 0 001.42 0C13.46 20.94 20 14.16 20 10a8 8 0 00-8-8z"/><path fill="#fff" d="M15.24 9.25l-1.75-1.74-1.07-1.08a.59.59 0 00-.83 0l-1.07 1.08-1.75 1.74L8 10a.58.58 0 00.41 1h7.16a.58.58 0 00.43-1l-.76-.75z"/></g></svg>
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    
+  <g fill-rule="evenodd">
+    <path fill="#006902" d="M12 2a8 8 0 0 0-8 8c0 4.16 6.54 10.94 7.29 11.7a1 1 0 0 0 1.42 0C13.46 20.94 20 14.16 20 10a8 8 0 0 0-8-8Z"/>
+    <path fill="#fff" d="m15.24 9.25-1.75-1.74-1.07-1.08a.59.59 0 0 0-.83 0l-1.07 1.08-1.75 1.74L8 10a.58.58 0 0 0 .41 1h7.16a.58.58 0 0 0 .43-1l-.76-.75Z"/>
+  </g>
+</svg>
