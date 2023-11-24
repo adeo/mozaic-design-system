cd1f5ad96733801d7a91ff0db33b070db160e06c
Charles-Antoine Queste <charlesantoinequeste@gmail.com>
2023-11-24 10:16:22 +0100

    feat(icons): update social icons

--- packages/icons/svelte/SocialNetworkFacebook48.svelte
+++ packages/icons/svelte/SocialNetworkFacebook48.svelte
@@ -7,4 +7,4 @@
   export let size = "3rem";
 </script>
 
-<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="M41 9v30a2 2 0 01-2 2h-8.54V27.84h4.41l.66-5.13h-5.07v-3.28a2.87 2.87 0 01.54-1.86 2.6 2.6 0 012-.57h2.7v-4.65a28.53 28.53 0 00-3.94-.2A6.6 6.6 0 0027 13.92a6.7 6.7 0 00-1.8 5v3.79h-4.48v5.13h4.43V41H9a2 2 0 01-2-2V9a2 2 0 012-2h30a2 2 0 012 2z"/></svg>
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="M27.125 43.716v-13.96h4.66l.886-5.777h-5.546v-3.748c0-1.579.773-3.121 3.26-3.121h2.522v-4.917s-2.289-.39-4.477-.39c-4.568 0-7.555 2.764-7.555 7.773v4.403h-5.08v5.777h5.08v13.96C11.314 42.218 4 33.952 4 23.98 4 12.945 12.954 4 24 4s20 8.945 20 19.98c0 9.972-7.314 18.238-16.875 19.736z"/></svg>
--- packages/icons/svelte/SocialNetworkFacebook64.svelte
+++ packages/icons/svelte/SocialNetworkFacebook64.svelte
@@ -7,4 +7,4 @@
   export let size = "4rem";
 </script>
 
-<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M55 11v42a2 2 0 01-2 2H40.74V37.19h6l.9-6.94h-6.9v-4.44a3.83 3.83 0 01.71-2.51 3.49 3.49 0 012.74-.84h3.65v-6.2a40 40 0 00-5.33-.26A8.88 8.88 0 0036 18.36a9 9 0 00-2.44 6.76v5.13h-6v6.94h6V55H11a2 2 0 01-2-2V11a2 2 0 012-2h42a2 2 0 012 2z"/></svg>
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M36.167 58.289V39.675h6.214l1.181-7.702h-7.395v-4.998c0-2.105 1.031-4.162 4.346-4.162h3.364v-6.556s-3.052-.52-5.97-.52c-6.09 0-10.073 3.686-10.073 10.365v5.87h-6.772v7.703h6.772v18.614c-12.748-1.998-22.5-13.02-22.5-26.316 0-14.713 11.94-26.64 26.667-26.64 14.727 0 26.666 11.927 26.666 26.64 0 13.296-9.751 24.318-22.5 26.316z"/></svg>
