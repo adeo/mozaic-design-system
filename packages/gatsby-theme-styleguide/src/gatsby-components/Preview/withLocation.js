1c9f2510acc1521de7529afb8f190caf010930bd
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-07 11:01:19 +0100

    docs(gatsby/theme): remove Preview component & all its associated elements
    [skip ci]

--- packages/gatsby-theme-styleguide/src/gatsby-components/Preview/withLocation.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Preview from './Preview'
-
-const WithLocation = (location) => {
-  return (props) => <Preview {...props} location={location} />
-}
-
-export default WithLocation
