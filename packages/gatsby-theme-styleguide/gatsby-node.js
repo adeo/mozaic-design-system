70a15584ae9ed2859b34389842ca40b569cbadf5
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-07 11:01:19 +0100

    docs(gatsby/theme): remove Preview component & all its associated elements
    [skip ci]

--- packages/gatsby-theme-styleguide/gatsby-node.js
+++ packages/gatsby-theme-styleguide/gatsby-node.js
@@ -4,8 +4,6 @@ const fs = require('fs')
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const postTemplate = path.resolve(`${__dirname}/src/templates/pattern-page.js`)
 
-let previewsPath = []
-
 /**
  * gatsby api hook that will run when the development server is started
  * adding this line will allow to extend the server development configuration.
@@ -46,16 +44,6 @@ exports.onCreateNode = ({ node, getNode, actions, reporter }) => {
       value: fileName,
     })
   }
-
-  if (node.internal.type === `Preview`) {
-    const slug = node.path.split('docs').pop()
-
-    createNodeField({
-      node,
-      name: `slug`,
-      value: slug,
-    })
-  }
 }
 
 // Utils
@@ -67,75 +55,6 @@ const debounce = (fnc, tm) => {
       fnc.apply(this, arguments)
     }, tm)
   }
-}
-
-// Create preview html
-const createPreviewHtmlFile = (node) =>
-  new Promise((resolve, reject) => {
-    const destDir = `public/previews${node.fields.slug
-      .split('/')
-      .slice(0, -1)
-      .join('/')}`
-
-    const fileName = node.fields.slug.split('/').pop()
-
-    fs.mkdir(destDir, { recursive: true }, (err) => {
-      if (err) reject(err)
-      fs.writeFile(`${destDir}/${fileName}.html`, buildHtml(node), (error) => {
-        if (error) reject(error)
-        resolve()
-      })
-    })
-  })
-
-// build previews for each node
-const createPreviewPages = (previews, previewsPath, reporter) => {
-  // Create previews html pages into static folder
-  const previewPromiseMap = previews.map(({ node }) => {
-    // Add path to the list for tests purposes if it does not exist
-    if (!previewsPath.includes(node.fields.slug)) {
-      previewsPath.push(node.fields.slug)
-    }
-
-    return createPreviewHtmlFile(node)
-  })
-
-  Promise.all(previewPromiseMap)
-    .then(() => {
-      reporter.success('preview files generated')
-    })
-    .catch((err) => reporter.panicOnBuild(err))
-}
-
-// Minimal builder for html previews
-const buildHtml = (data) => {
-  var generatedStyles = data.codes.css
-  var body = data.codes.html
-  return `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8"><meta http-equiv="x-ua-compatible" content="ie=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-          <style type="text/css">
-            * {
-              box-sizing: padding-box;
-            }
-            
-            body, html {
-              margin :0;
-              padding :0;
-              display: block;
-              overflow: hidden
-            }
-          </style>
-          <style>${generatedStyles}</style>
-        </head>
-        <body>
-          ${body}
-        </body>
-      </html>
-    `
 }
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
@@ -150,18 +69,6 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
           }
           internal {
             contentFilePath
-          }
-        }
-      }
-      allPreview {
-        nodes {
-          codes {
-            css
-            html
-            js
-          }
-          fields {
-            slug
           }
         }
       }
@@ -181,7 +88,4 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       },
     })
   })
-
-  const previews = result.data.allPreview.nodes
-  debounce(createPreviewPages(previews, previewsPath, reporter), 1000)
 }
