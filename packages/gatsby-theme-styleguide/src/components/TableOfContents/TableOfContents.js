1eaf4acfd41ec688b2cf80a63bb8b13848b8714e
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-08 17:55:28 +0100

    docs(gatsby/theme): refactor component 'TableOfContents'
    [skip ci]

--- packages/gatsby-theme-styleguide/src/components/TableOfContents/TableOfContents.js
+++ packages/gatsby-theme-styleguide/src/components/TableOfContents/TableOfContents.js
@@ -1,140 +1,91 @@
-import React, { PureComponent } from 'react'
-import copyToClipBoard from '../../utils/copy-to-clipboard'
-import './tableofcontents.scss'
+import * as React from 'react'
+import * as styles from './toc.module.css'
 
-export default class TableOfContents extends PureComponent {
-  getPageTabsHeight = () => {
-    const pageTabs = document.querySelector('#page_tabs_menu')
-    return pageTabs ? pageTabs.offsetHeight + 5 : 0
-  }
+function getPageTabsHeight() {
+  const pageTabs = document.querySelector('#pageTabs')
+  return pageTabs ? pageTabs.offsetHeight + 5 : 0
+}
 
-  scroller = ({ hash }) => {
-    hash = hash.replace('#', '')
-    hash = decodeURIComponent(hash)
-    const clientRects = document
-      .querySelector(`[id="${hash}"]`)
-      .getBoundingClientRect()
-    const pageTabsHeight = this.getPageTabsHeight()
-    const offset = pageTabsHeight || 5
-    const scrollingElem = document.getElementById('scroller')
-    scrollingElem.scrollTop = clientRects.y + scrollingElem.scrollTop - offset
-  }
+function scroller(hash) {
+  hash = hash.replace('#', '')
+  hash = decodeURIComponent(hash)
+  const clientRects = document
+    .querySelector(`[id="${hash}"]`)
+    .getBoundingClientRect()
 
-  onClick = (e) => {
+  const pageTabsHeight = getPageTabsHeight()
+  const offset = pageTabsHeight || 5
+  const scrollingElem = document.getElementById('scroller')
+  scrollingElem.scrollTop = clientRects.y + scrollingElem.scrollTop - offset
+}
+
+const TocLink = (props) => {
+  const { url, title } = props
+
+  function handleClick(e) {
     e.preventDefault()
     const {
       target: { hash },
     } = e
-    this.scroller({ hash })
+
+    scroller(hash)
     return false
   }
 
-  getAnchorPosition = (e) => {
-    const scrollValue = document.getElementById('page_tabs_menu')
-      ? document.getElementById('scroller').scrollTop +
-        document.getElementById('page_tabs_menu').clientHeight +
-        20
-      : 20
+  return (
+    <a className={styles.tocLink} href={url} onClick={handleClick}>
+      {title}
+    </a>
+  )
+}
 
-    const anchors = document.querySelectorAll('.anchor-toc')
+const TocItems = (props) => {
+  const { list } = props
 
-    anchors.forEach(function (anchor, index, array) {
-      const position = anchor.offsetTop
-      const hash = anchor.href.match(/#[\w-_]+$/)[0]
+  return list.map((element, index) => {
+    let { url, title, items } = element
 
-      if (!(index < anchors.length - 1)) return false
-
-      const nextPosition = anchors[index + 1].offsetTop
-
-      if (scrollValue >= position && scrollValue < nextPosition) {
-        document.querySelectorAll('.toc__link').forEach((link) => {
-          if (link.classList.contains('toc__link--active')) {
-            link.classList.remove('toc__link--active')
-            link.blur()
-          }
-        })
-
-        document
-          .querySelector('.toc__link[href="' + hash + '"')
-          .classList.add('toc__link--active')
-      }
-    })
-  }
-
-  componentDidMount() {
-    document
-      .getElementById('scroller')
-      .addEventListener('scroll', this.getAnchorPosition)
-
-    const pageTabsHeight = this.getPageTabsHeight()
-    const tocDiv = document.querySelector('#toc > div')
-    if (tocDiv) tocDiv.style.top = pageTabsHeight + 11 + 'px'
-
-    const hash = window.location.href.match(/#[\w-_]+$/)
-    if (hash) {
-      setTimeout(() => {
-        this.scroller({ hash: hash[0] })
-      }, 800)
-    }
-
-    document.querySelectorAll('.anchor-toc').forEach((elem) => {
-      elem.title = 'Click to copy the link'
-      elem.addEventListener('click', (e) => {
-        e.preventDefault()
-        copyToClipBoard(e.currentTarget.href)
+    // parsing the {#id} (optional)
+    if (title) {
+      title = title.replace(/\{#([\w-]+)\}$/, (_, capture) => {
+        url = '#' + capture
+        return ''
       })
-    })
-  }
-
-  renderList = (list) =>
-    list.map((elem, index) => {
-      let { url, title, items } = elem
-      if (items) {
-        items = (
-          <ul className="toc__list" key={'group_' + index}>
-            {this.renderList(items)}
-          </ul>
-        )
-      }
-
-      // parsing the {#id} (optional)
-      let anchor = false
-      if (title) {
-        title = title.replace(/\{#([\w-]+)\}$/, (_, capture) => {
-          url = '#' + capture
-          return ''
-        })
-        title = title.trim()
-        anchor = (
-          <a className="toc__link" onClick={this.onClick} href={url}>
-            {title}
-          </a>
-        )
-      }
-      return (
-        <li className="toc__item" key={url + '_link_' + index}>
-          {anchor}
-          {items}
-        </li>
-      )
-    })
-
-  render() {
-    const { tableOfContents = {} } = this.props
-
-    if (!tableOfContents.items) {
-      return false
+      title = title.trim()
     }
 
     return (
-      <aside className="toc" id="toc">
-        <div className="toc__wrapper">
-          <h2 className="toc__title">On this page</h2>
-          <ul className="toc__list">
-            {this.renderList(tableOfContents.items)}
-          </ul>
-        </div>
-      </aside>
+      <li className={styles.tocItem} key={url + '_link_' + index}>
+        {title && <TocLink url={url} title={title} />}
+        {items && <TocList list={items} key={'group_' + index} />}
+      </li>
     )
-  }
+  })
 }
+
+const TocList = (props) => {
+  return (
+    <ul className={styles.tocList}>
+      <TocItems {...props} />
+    </ul>
+  )
+}
+
+const TableOfContents = (props) => {
+  const { tableOfContents } = props
+
+  if (!tableOfContents.items) {
+    return false
+  }
+
+  return (
+    <aside className={styles.toc} id="toc">
+      <div className={styles.tocWrapper}>
+        <div className={styles.tocTitle}>On this page</div>
+        <TocList list={tableOfContents.items} />
+      </div>
+    </aside>
+  )
+}
+
+export default TableOfContents
