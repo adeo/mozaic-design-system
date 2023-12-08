3d750dfa83288e407e271d0a2bfaec4c0a694668
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-08 12:13:43 +0100

    docs(gatsby/theme): refactor component 'Footer'
    [skip ci]

--- packages/gatsby-theme-styleguide/src/components/Footer/Footer.js
+++ packages/gatsby-theme-styleguide/src/components/Footer/Footer.js
@@ -1,50 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
+import * as React from 'react'
 import logoAdeo from '../../images/Adeo_logo.jpg'
 import logoLeroyMerlin from '../../images/Leroy_Merlin.jpg'
-
-const FooterContainer = styled.footer`
-  align-items: center;
-  border-top: 1px solid #000000;
-  display: flex;
-  justify-content: center;
-  min-height: 11.25rem;
-`
-
-const FooterContent = styled.p`
-  font-size: 0.875rem;
-  line-height: 1.143;
-  margin: 0;
-  text-align: center;
-
-  span {
-    display: block;
-    margin-bottom: 1rem;
-  }
-
-  img {
-    display: inline-block;
-
-    &:first-of-type {
-      margin-right: 1rem;
-      max-width: 53px;
-    }
-
-    &:last-of-type {
-      max-width: 62px;
-    }
-  }
-`
+import * as styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <FooterContainer>
-      <FooterContent>
+    <footer className={styles.container}>
+      <p className={styles.content}>
         <span>Mozaic, a product made by</span>
         <img src={logoLeroyMerlin} alt="Leroy Merlin" />
         <img src={logoAdeo} alt="Adeo" />
-      </FooterContent>
-    </FooterContainer>
+      </p>
+    </footer>
   )
 }
 
