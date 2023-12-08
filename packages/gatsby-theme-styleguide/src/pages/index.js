6e3b70e3acf2e7296012b9be34051d422aec8b6a
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-08 13:16:16 +0100

    docs(gatsby/theme): refactor page 'Home'
    [skip ci]

--- packages/gatsby-theme-styleguide/src/pages/index.js
+++ packages/gatsby-theme-styleguide/src/pages/index.js
@@ -1,101 +1,34 @@
 import * as React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
-import styled from 'styled-components'
-import AreaContact from '../components/AreaContact'
+import Layout from '../components/layout'
+import Header from '../components/Header'
 import AreaDiscover from '../components/AreaDiscover'
+import AreaContact from '../components/AreaContact'
 import AreaValues from '../components/AreaValues'
 import Footer from '../components/Footer'
-import Header from '../components/Header'
-import Menu from '../components/Menu'
-import JSONData from '../../../../src/content/datas.json'
 import './index.scss'
 
-// --------- COMMON
-const Global = styled.div`
-  display: flex;
-  align-content: stretch;
-  height: 100vh;
-  width: 100vw;
-  overflow-y: hidden;
-`
-
-const Main = styled.main`
-  flex: 1;
-  scroll-behavior: smooth;
-  overflow-y: auto;
-  overflow-x: hidden;
-  position: relative;
-
-  .nav-open & {
-    overflow: hidden;
-  }
-`
-
-const AsideContainer = styled.aside`
-  background: #eeeef0;
-  min-height: 100vh;
-  position: relative;
-  width: 18.75rem;
-
-  @media screen and (max-width: 1023px) {
-    left: 0;
-    position: fixed;
-    top: 0;
-    transition: all 0.4s;
-    transform: translateX(-100%);
-    z-index: 1000;
-  }
-
-  .nav-open & {
-    transform: translateX(0);
-  }
-`
-
-// --------- PAGE
-const IndexPage = ({ release, discover, contactus, ourvalues }) => {
+const IndexPage = ({ location }) => {
   return (
-    <Global>
-      <AsideContainer>
-        <Menu siteTitle={'Mozaic'} />
-      </AsideContainer>
-      <Main>
-        <Header />
-        <AreaDiscover cards={discover} />
-        <AreaContact cards={contactus} />
-        <AreaValues values={ourvalues} />
-        <Footer />
-      </Main>
-    </Global>
+    <Layout location={location}>
+      <Header />
+      <AreaDiscover />
+      <AreaContact />
+      <AreaValues />
+      <Footer />
+    </Layout>
   )
 }
 
-const Index = () => {
-  const data = useStaticQuery(graphql`
-    query currentRelase {
-      githubRelease(isCurrent: { eq: true }) {
-        tagName
-      }
-    }
-  `)
-
+export const Head = (props) => {
   return (
-    <IndexPage
-      release={data.githubRelease.tagName}
-      discover={JSONData.discover}
-      contactus={JSONData.contactus}
-      ourvalues={JSONData.ourvalues}
-    />
+    <>
+      <title>Mozaic – the Adeo's Design System</title>
+      <meta
+        name="description"
+        content="Mozaic is Adeo's Design System, created to enhance the team's delivery time, improve quality of deliverables and thus help set up a coherent customer experience."
+      />
+    </>
   )
 }
 
-export default Index
-
-export const Head = () => (
-  <>
-    <title>Mozaic Design System</title>
-    <meta
-      name="description"
-      content="Mozaic is Adeo's Design System, created to enhance the team's delivery time, improve quality of deliverables and thus help set up a coherent customer experience."
-    />
-  </>
-)
+export default IndexPage
