0353975944ac78f4fb7296a3b985aef8bd2224b7
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-08 17:14:29 +0100

    docs(gatsby/theme): refactor component 'PageTabs'
    [skip ci]

--- packages/gatsby-theme-styleguide/src/components/PageTabs/PageTabs.js
+++ packages/gatsby-theme-styleguide/src/components/PageTabs/PageTabs.js
@@ -1,113 +1,33 @@
-import React, { PureComponent } from 'react'
+import * as React from 'react'
 import { Link } from 'gatsby'
-import styled from 'styled-components'
-import { MagicUnit } from '@mozaic-ds/tokens/build/js/tokens.js'
+import { setSlugify } from '../../utils'
+import * as styles from './tabs.module.css'
 
-const Tabs = styled.div`
-  position: sticky;
-  width: 100%;
-  z-index: 1;
-  top: 0;
-  border-bottom: solid 1px #000;
-  background: white;
+const PageTabs = (props) => {
+  const { tabs } = props
 
-  @media screen and (max-width: 767px) {
-    overflow-x: auto;
-  }
-`
+  return (
+    <div id="pageTabs" className={styles.container}>
+      <div className={styles.wrapper}>
+        {tabs.map((node) => {
+          const slug = setSlugify(node.slug)
+          const { title } = node
 
-const TabsWrapper = styled.div`
-  display: inline-flex;
-  padding-left: ${MagicUnit * 1.5}rem;
-  max-width: ${MagicUnit * 52}rem;
-
-  @media screen and (min-width: 768px) {
-    padding-left: ${MagicUnit * 3}rem;
-  }
-`
-
-const TabItem = styled.div``
-
-const TabLink = styled(Link)`
-  color: #000;
-  display: block;
-  position: relative;
-  padding: ${MagicUnit}rem ${MagicUnit * 1.5}rem;
-
-  &::after {
-    content: '';
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 0.25rem;
-  }
-
-  &:hover:not(.is-active) {
-    &::after {
-      background: rgba(0, 0, 0, 0.5);
-    }
-  }
-
-  &.is-active {
-    font-weight: bold;
-
-    &::after {
-      background: rgba(0, 0, 0, 1);
-    }
-  }
-`
-
-class PageTabs extends PureComponent {
-  orderPageTab = (pageTabs) => {
-    // order using frontmatter order tag and remove index
-    const orderedTabs = [...pageTabs]
-      .sort((a, b) => {
-        return a.node.frontmatter.order - b.node.frontmatter.order
-      })
-      .filter((tab) => tab.node.fields.fileName.name !== 'index')
-
-    const cleanIndex = [...pageTabs].find(
-      (tab) => tab.node.fields.fileName.name === 'index'
-    )
-
-    const newArr = [
-      {
-        title: 'Usage', // rename index tab 'Usage'
-        slug: cleanIndex.node.fields.slug,
-      },
-      ...orderedTabs.map((tab) => ({
-        title: tab.node.frontmatter.title,
-        slug: tab.node.fields.slug,
-      })),
-    ]
-
-    return newArr
-  }
-
-  render() {
-    const { samePageTabs } = this.props
-    const cleanTabs = this.orderPageTab(samePageTabs)
-    return (
-      <Tabs id="page_tabs_menu">
-        <TabsWrapper>
-          {cleanTabs.map((node) => (
-            <TabItem key={node.slug}>
-              <TabLink
-                to={node.slug}
-                activeClassName="is-active"
-                state={{
-                  isCode: node.title === 'Code',
-                }}
+          return (
+            <div className={styles.tabItem} key={slug}>
+              <Link
+                to={slug}
+                className={styles.tabLink}
+                activeClassName={styles.isActive}
               >
-                {node.title}
-              </TabLink>
-            </TabItem>
-          ))}
-        </TabsWrapper>
-      </Tabs>
-    )
-  }
+                {title}
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default PageTabs
