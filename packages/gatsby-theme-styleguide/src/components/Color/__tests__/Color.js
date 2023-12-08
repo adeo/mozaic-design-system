5c5936e62ca9f0053f763466ca414f6944fae2b4
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-08 14:10:17 +0100

    docs(gatsby/theme): refactor component 'Swatch' & rename it as 'ColorPalette'
    [skip ci]

--- packages/gatsby-theme-styleguide/src/components/Color/__tests__/Color.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import Color from '../Color'
-
-test('<Color />', () => {
-  const tree = renderer.create(<Color color="blue" />).toJSON()
-  expect(tree).toMatchSnapshot()
-})
