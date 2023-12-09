96e03c4b2c683fbc8ac0a7c45e74c5b3f590cad6
Trésor ILOYI <tresor.iloyi@gmail.com>
2023-12-09 09:51:03 +0100

    chore(gatsby/config): remove unused Gatsby files/elements

--- packages/gatsby-theme-styleguide/gatsby-node.js
+++ packages/gatsby-theme-styleguide/gatsby-node.js
@@ -1,22 +1,9 @@
-const express = require('express')
 const path = require('path')
-const fs = require('fs')
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const colorsTokensLM =
   require('@mozaic-ds/tokens/build/js/tokensObject.js').color
 const colorsTokensAdeo =
   require('@mozaic-ds/tokens/buildAdeo/js/tokensObject.js').color
-
-/**
- * gatsby api hook that will run when the development server is started
- * adding this line will allow to extend the server development configuration.
- * For this special case it will allow the content inside the public folder that gatsby will generate to be served and accessible.
- * more on that here =>https://www.gatsbyjs.org/docs/node-apis/#onCreateDevServer
- *
- */
-exports.onCreateDevServer = ({ app }) => {
-  app.use(express.static('public'))
-}
 
 exports.onCreateNode = ({ node, getNode, actions, reporter }) => {
   const { createNodeField } = actions
@@ -46,17 +33,6 @@ exports.onCreateNode = ({ node, getNode, actions, reporter }) => {
       name: `fileName`,
       value: fileName,
     })
-  }
-}
-
-// Utils
-const debounce = (fnc, tm) => {
-  let time
-  return function () {
-    clearTimeout(time)
-    time = setTimeout(() => {
-      fnc.apply(this, arguments)
-    }, tm)
   }
 }
 
