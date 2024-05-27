7bf92d5b1d96c3264eca2d25a40a212c304f476d
Charles-Antoine Queste <charlesantoinequeste@gmail.com>
2024-05-27 15:40:39 +0200

    feat(icons): add new icons

---
+++ packages/icons/svelte/SocialNetworkTiktok32.svelte
@@ -0,0 +1,10 @@
+<svelte:options tag={null} />
+  <script>
+  export let id = undefined;
+  export let style = undefined;
+  export let className = undefined;
+  export let fill = undefined;
+  export let size = "2rem";
+</script>
+
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g clip-path="url(#a)"><path d="M23.7 8.012a6.392 6.392 0 01-2.893-5.345H16.24l-.007 18.374c-.077 2.057-1.763 3.709-3.83 3.709a3.795 3.795 0 01-1.781-.443 3.858 3.858 0 01-2.057-3.411c0-2.126 1.721-3.854 3.837-3.854.395 0 .774.065 1.133.178v-4.68a8.348 8.348 0 00-1.133-.083C7.77 12.457 4 16.242 4 20.895a8.443 8.443 0 003.587 6.91 8.33 8.33 0 004.816 1.529c4.634 0 8.403-3.786 8.403-8.439v-9.317a10.825 10.825 0 006.352 2.052V9.045a6.292 6.292 0 01-3.459-1.033z"/></g><defs><clipPath id="a"><path d="M0 0h32v32H0z"/></clipPath></defs></svg>
