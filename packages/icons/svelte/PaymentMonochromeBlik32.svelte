4f862560215d91d898e69e56987fabed9a07b3b4
Charles-Antoine Queste <charlesantoinequeste@gmail.com>
2024-06-20 17:14:28 +0200

    feat(icons): update icons and logotypes

---
+++ packages/icons/svelte/PaymentMonochromeBlik32.svelte
@@ -0,0 +1,10 @@
+<svelte:options tag={null} />
+  <script>
+  export let id = undefined;
+  export let style = undefined;
+  export let className = undefined;
+  export let fill = undefined;
+  export let size = "2rem";
+</script>
+
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 48"><path d="M28.096 34.596a4.959 4.959 0 110-9.918 4.959 4.959 0 010 9.918z"/><path fill-rule="evenodd" d="M8.333 2H90.47a5.43 5.43 0 015.427 5.42v33.16A5.43 5.43 0 0190.47 46H8.333a5.43 5.43 0 01-5.426-5.42V7.42A5.43 5.43 0 018.333 2zm30.066 10.236a4.86 4.86 0 10-9.719-.063 4.86 4.86 0 009.719.063zm-10.303 7.1c-1.7-.003-3.375.417-4.873 1.222V8.904h-5.432v20.733a10.304 10.304 0 1010.305-10.302zM42.218 8.902h5.432v30.82h-5.432V8.904zm15.684 10.634H52.47v20.187h5.432V19.537zm24.866 20.188H75.77l-7.623-10.773.004 10.773h-5.433V8.907h5.433v20.019l7.486-9.388h6.346l-7.623 9.33 8.407 10.857z"/></svg>
