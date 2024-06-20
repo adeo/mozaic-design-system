f6b2900144cb3fb825787312ec5e18c7b6fdbb59
Charles-Antoine Queste <43212270+caqueste@users.noreply.github.com>
2024-06-20 22:03:31 +0200

    feat(icons): update icons and logotypes (#1599)

---
+++ packages/icons/svelte/Transfer48.svelte
@@ -0,0 +1,10 @@
+<svelte:options tag={null} />
+  <script>
+  export let id = undefined;
+  export let style = undefined;
+  export let className = undefined;
+  export let fill = undefined;
+  export let size = "3rem";
+</script>
+
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path fill-rule="evenodd" d="M34.062 4.937A1.5 1.5 0 1031.94 7.06l6.44 6.439H10a1.5 1.5 0 000 3h28.38l-6.44 6.44a1.5 1.5 0 102.122 2.12l9-9a1.5 1.5 0 000-2.12l-9-9zm-18 20.122a1.5 1.5 0 10-2.12-2.122l-9 9a1.5 1.5 0 000 2.122l9 9a1.5 1.5 0 102.12-2.122l-6.439-6.439h28.379a1.5 1.5 0 100-3H9.623l6.44-6.44z"/></svg>
