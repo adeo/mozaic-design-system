67cc9427d9a90e5fda6fd49a544c7e7350ee776f
Charles-Antoine Queste <charlesantoinequeste@gmail.com>
2024-10-21 17:45:18 +0200

    feat(icons): add new icons

---
+++ packages/icons/svelte/CardLoyaltym64.svelte
@@ -0,0 +1,10 @@
+<svelte:options tag={null} />
+  <script>
+  export let id = undefined;
+  export let style = undefined;
+  export let className = undefined;
+  export let fill = undefined;
+  export let size = "4rem";
+</script>
+
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path fill-rule="evenodd" d="M8 11.833a1.5 1.5 0 00-1.5 1.5v37.334a1.5 1.5 0 001.5 1.5h48a1.5 1.5 0 001.5-1.5V13.333a1.5 1.5 0 00-1.5-1.5H8zm1.5 37.334V14.833h45v34.334h-45zm14.933-27.042l7.239 7.228a.465.465 0 00.656 0l7.239-7.228a.464.464 0 00-.329-.792H24.762c-.414 0-.621.5-.329.792zm-4.428 20.01l2.57-17.11a.465.465 0 01.788-.259l8.309 8.297a.465.465 0 00.656 0l8.31-8.297a.465.465 0 01.787.26l2.57 17.108a.464.464 0 01-.46.533h-5.56a.464.464 0 01-.459-.398l-.74-5.177a.465.465 0 00-.789-.262l-3.659 3.654a.465.465 0 01-.656 0l-3.66-3.654a.465.465 0 00-.788.262l-.74 5.177a.464.464 0 01-.46.398h-5.56a.464.464 0 01-.459-.533z"/></svg>
