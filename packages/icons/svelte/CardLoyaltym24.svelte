49a205f4954d06ec01ac0d6cb4e56684f3274b34
Charles-Antoine Queste <43212270+caqueste@users.noreply.github.com>
2024-10-22 08:45:32 +0200

    feat(icons): add new icons (#1618)
    Co-authored-by: Mohamed Mokhtari <mohamed.mokhtari.pro@gmail.com>

---
+++ packages/icons/svelte/CardLoyaltym24.svelte
@@ -0,0 +1,10 @@
+<svelte:options tag={null} />
+  <script>
+  export let id = undefined;
+  export let style = undefined;
+  export let className = undefined;
+  export let fill = undefined;
+  export let size = "1.5rem";
+</script>
+
+<svg aria-hidden="true" width={size} height={size} id={id}  style={style} class={className} fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M3 4a1 1 0 00-1 1v14a1 1 0 001 1h18a1 1 0 001-1V5a1 1 0 00-1-1H3zm1 14V6h16v12H4zm5.163-9.703l2.714 2.71a.174.174 0 00.246 0l2.715-2.71A.174.174 0 0014.713 8H9.286a.174.174 0 00-.123.297zM7.502 15.8l.964-6.416a.174.174 0 01.295-.097l3.116 3.112a.174.174 0 00.246 0l3.116-3.112a.174.174 0 01.295.097l.964 6.416a.174.174 0 01-.172.2H14.24a.174.174 0 01-.172-.15l-.278-1.94a.174.174 0 00-.296-.099l-1.372 1.37a.174.174 0 01-.246 0l-1.372-1.37a.174.174 0 00-.296.098l-.277 1.942a.174.174 0 01-.173.149H7.674a.174.174 0 01-.172-.2z"/></svg>
